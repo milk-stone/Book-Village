fa21958935277cde7d049b2cadf83f033b98e116
milk-stone <ywshero@naver.com>
2025-01-31 17:08:00 +0900

    init: 기본 Spring 프로젝트 설정, .gitignore 내용 추가

---
+++ backend/src/test/java/com/bookvillage/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookvillage.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
