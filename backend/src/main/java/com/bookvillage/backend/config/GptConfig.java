73a7c2ab1e0148ea4a703985e8ed27ff13eaa9ee
milk-stone <ywshero@naver.com>
2025-02-06 15:33:18 +0900

    feat: GptConfig 작성
    Gpt API를 사용할 때 전송해야하는 HttpHeaders를 Bean으로 등록함.
    API Key와 모델 정보는 환경 변수로 빼놓음

---
+++ backend/src/main/java/com/bookvillage/backend/config/GptConfig.java
@@ -0,0 +1,33 @@
+package com.bookvillage.backend.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+
+
+@Getter
+@Configuration
+public class GptConfig {
+    @Value("${openai.model")
+    private String openaiModel;
+
+    @Value("${openai.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public HttpHeaders httpHeadersForGpt(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth("Bearer " + secretKey);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
+    }
+}
